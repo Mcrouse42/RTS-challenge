31f8480fd15d7c18c7f8ad761e07d6fdb5455b84
Mcrouse <matthew.crouse@temple.edu>
2021-08-20 17:28:17 -0400

    added function to use chromedriver to search for RTS labs

---
+++ index.js
@@ -0,0 +1,24 @@
+var webdriver = require('selenium-webdriver'),
+      By = webdriver.By,
+      until = webdriver.until;
+
+var driver = new webdriver.Builder()
+      .forBrowser('chrome')
+      .build();
+
+driver.get('http://www.google.com/ncr');
+driver.findElement(By.name('q')).sendKeys('RTS labs');
+driver.wait(check_title, 1000);
+
+function check_title() {
+      var promise = driver.getTitle().then(function (title) {
+            if (title === 'RTS labs - Google Search') {
+                  console.log('success');
+                  return true;
+            }
+            else {
+                  console.log('fail -- ' + title);
+            }
+      });
+      return promise;
+}
